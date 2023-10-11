055a9d8d68a15f9ae2385ed05025fddcdc9e0bee
fadida5 <fadidaniv5@gmail.com>
2023-10-11 21:47:01 +0300

    signin_up

--- backend/models/authentication/user.js
+++ backend/models/authentication/user.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const { v4: uuidv4 } = require('uuid');
-const {ObjectId} = mongoose.Schema;
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
+const { ObjectId } = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-    name: {type: String,trim: true,required: true,maxlength: 32},
-    lastname: {type: String,trim: true,required: true},
-    // hashed_password: {type: String,required: true},
-    // salt: String,
-    personalnumber: {type: String,trim: true,unique:true,require:true},
-    role:{type: String,default:"0"},
-    validated:{type: Boolean,default:false},
-    //
-    gdodid: {type:String},
-    hativaid: {type:String},
-    ogdaid: {type:String},
-    pikodid: {type:String},
-    //
-    site_permission: {type:String,default:'צפייה ועריכה'},
-    //
-    mainscreenid: {type:String},
-}, {timestamps: true})
+const userSchema = new mongoose.Schema(
+	{
+		// hashed_password: {type: String,required: true},
+		// salt: String,
+		personalnumber: { type: String, trim: true, unique: true, require: true },
+		role: { type: String, default: "0" },
+		validated: { type: Boolean, default: true },
+		unit: { type: String },
+		//
+		//
+		site_permission: { type: String, default: "צפייה ועריכה" },
+		//
+		// mainscreenid: {type:String},
+	},
+	{ timestamps: true }
+);
 
 // virtual field
 // userSchema.virtual('password')
